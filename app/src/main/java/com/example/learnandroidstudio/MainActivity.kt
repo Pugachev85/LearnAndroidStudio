bfc46097b6e7ace123233d705b8fe455cf2318f0
Pugachev85 <workpost2009@mail.ru>
2023-09-18 22:22:51 +0300

    Lesson 2

--- app/src/main/java/com/example/learnandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/learnandroidstudio/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.learnandroidstudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,16 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val label = findViewById<TextView>(R.id.main_label)
+        val listView = findViewById<ListView>(R.id.listView)
         val userData: EditText = findViewById(R.id.user_data)
         val button: Button = findViewById(R.id.button)
 
+        val todos: MutableList<String> = mutableListOf()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todos)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener{adapterView,  view,  i, l ->
+            val text = listView.getItemAtPosition(i).toString()
+            adapter.remove(text)
+            Toast.makeText(this, "Мы удалили: $text", Toast.LENGTH_LONG).show()
+        }
+
         button.setOnClickListener{
             val text = userData.text.toString().trim()
-            if (text == "toast")
-                Toast.makeText(this, "Useer enter toast", Toast.LENGTH_SHORT).show()
-            else
-                label.text = text
+            if (text != "")
+                adapter.insert(text, 0)
         }
     }
 }
