0537c25c120c8df14bef21353dc899b73fb1500a
Pugachev85 <workpost2009@mail.ru>
2023-10-01 23:00:22 +0300

    Lesson 3 add layout

--- app/src/main/java/com/example/learnandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/learnandroidstudio/MainActivity.kt
@@ -14,24 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView = findViewById<ListView>(R.id.listView)
-        val userData: EditText = findViewById(R.id.user_data)
-        val button: Button = findViewById(R.id.button)
-
-        val todos: MutableList<String> = mutableListOf()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todos)
-        listView.adapter = adapter
-
-        listView.setOnItemClickListener{adapterView,  view,  i, l ->
-            val text = listView.getItemAtPosition(i).toString()
-            adapter.remove(text)
-            Toast.makeText(this, "Мы удалили: $text", Toast.LENGTH_LONG).show()
-        }
-
-        button.setOnClickListener{
-            val text = userData.text.toString().trim()
-            if (text != "")
-                adapter.insert(text, 0)
-        }
+        val userLogin : TextView = findViewById(R.id.user_login)
+        val userEmail : TextView = findViewById(R.id.user_email)
+        val userPass: TextView = findViewById(R.id.user_pass)
     }
 }
